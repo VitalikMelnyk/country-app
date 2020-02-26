dfe6dea9565b05906af942c5a8ad25fe86577d87
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-02-26 15:05:25 +0200

    Updates

---
+++ precache-manifest.ecdd79adf5e6c79c632aa379d66b2fe7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c914a97ce9004d2fd00debf5e2d3a14e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc8687be196f00475c0e",
+    "url": "/static/css/2.3b35c00c.chunk.css"
+  },
+  {
+    "revision": "34f0fee0a34f7d1696bd",
+    "url": "/static/css/main.d35325ed.chunk.css"
+  },
+  {
+    "revision": "fc8687be196f00475c0e",
+    "url": "/static/js/2.8ce2f247.chunk.js"
+  },
+  {
+    "revision": "b655912b7b5f89b32023a3c857e9675b",
+    "url": "/static/js/2.8ce2f247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f0fee0a34f7d1696bd",
+    "url": "/static/js/main.00385767.chunk.js"
+  },
+  {
+    "revision": "44dadc0095665388493f",
+    "url": "/static/js/runtime-main.7f842d0a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
